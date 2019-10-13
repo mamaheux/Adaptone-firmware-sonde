7a9a7b6d82e75d00e2fdc9594370329f2d117bac
marc-antoine <maheux.marcantoine@gmail.com>
2019-10-13 11:09:49 -0400

    Ajout de l'initialisation du service SNTP et du log du temps.

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,15 +1,35 @@
 #include "config.h"
 #include "event.h"
 #include "network/ethernet.h"
+#include "network/sntp.h"
 #include "network/discovery.h"
 
 #include <freertos/FreeRTOS.h>
+
+#include <time.h>
+
+#define STRFTIME_BUFFER_SIZE 64
+#define UTC_LOG_INTERVAL_MS 10000
 
 static void initializeLogger()
 {
     esp_log_level_set(MAIN_LOGGER_TAG, MAIN_LOGGER_LEVEL);
     esp_log_level_set(EVENT_LOGGER_TAG, EVENT_LOGGER_LEVEL);
     esp_log_level_set(NETWORK_LOGGER_TAG, NETWORK_LOGGER_LEVEL);
+}
+
+static void logCurrentUtc()
+{
+    char buffer[STRFTIME_BUFFER_SIZE];
+    struct timeval tv;
+    struct tm timeinfo;
+
+    if (gettimeofday(&tv, NULL) == 0)
+    {
+        localtime_r(&tv.tv_sec, &timeinfo);
+        strftime(buffer, sizeof(buffer), "%c", &timeinfo);
+        ESP_LOGI(MAIN_LOGGER_TAG, "The current UTC time is: %s (us = %ld)", buffer, tv.tv_usec);
+    }
 }
 
 void app_main()
@@ -20,12 +40,14 @@ void app_main()
     ESP_LOGI(MAIN_LOGGER_TAG, "Initialization");
     initializeEvent();
     initializeEthernet();
+    initializeStnp();
     initializeDiscovery();
 
     ESP_LOGI(MAIN_LOGGER_TAG, "Task start");
     startDiscovery();
     while(1)
     {
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        logCurrentUtc();
+        vTaskDelay(UTC_LOG_INTERVAL_MS / portTICK_PERIOD_MS);
     }
 }
