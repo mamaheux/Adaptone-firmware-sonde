ccb19e73726180d1f9fd85acf1982c1e3f8ee376
marc-antoine <maheux.marcantoine@gmail.com>
2019-11-02 16:27:18 -0400

    Ajout de constantes

--- firmware/src/sound.c
+++ firmware/src/sound.c
@@ -14,8 +14,15 @@
 #define UDP_MESSAGE_PAYLOAD_HEADER_SIZE 9
 #define UDP_MESSAGE_SIZE (UDP_MESSAGE_FULL_HEADER_SIZE + CONFIG_SOUND_MESSAGE_SAMPLE_COUNT * sizeof(int32_t))
 #define UDP_MESSAGE_ID 7
+#define UDP_MESSAGE_CURRENT_PAYLOAD_SIZE_OFFSET 4
+#define UDP_MESSAGE_CURRENT_ID_OFFSET 8
+#define UDP_MESSAGE_CURRENT_HOUR_OFFSET 10
+#define UDP_MESSAGE_CURRENT_MINUTE_OFFSET 11
+#define UDP_MESSAGE_CURRENT_SECOND_OFFSET 12
+#define UDP_MESSAGE_CURRENT_MS_OFFSET 13
+#define UDP_MESSAGE_CURRENT_US_OFFSET 15
 
-static const ledc_timer_config_t LEDC_TIMER_CONFIG = 
+static const ledc_timer_config_t LEDC_TIMER_CONFIG =
 {
     .speed_mode = LEDC_HIGH_SPEED_MODE,
     .timer_num  = LEDC_TIMER_0,
@@ -23,7 +30,7 @@ static const ledc_timer_config_t LEDC_TIMER_CONFIG =
     .freq_hz    = 11289600
 };
  
-static const ledc_channel_config_t LEDC_CHANNEL_CONFIG = 
+static const ledc_channel_config_t LEDC_CHANNEL_CONFIG =
 {
     .channel    = LEDC_CHANNEL_0,
     .gpio_num   = CONFIG_SOUND_CLOCK_IO,
@@ -32,7 +39,7 @@ static const ledc_channel_config_t LEDC_CHANNEL_CONFIG =
     .duty       = 2
 };
 
-static const gpio_config_t ADC_IO_CONFIG = 
+static const gpio_config_t ADC_IO_CONFIG =
 {
     .intr_type = GPIO_PIN_INTR_DISABLE,
     .mode = GPIO_MODE_OUTPUT,
@@ -41,7 +48,7 @@ static const gpio_config_t ADC_IO_CONFIG =
     .pull_up_en = 0
 };
 
-static const i2s_config_t I2S_CONFIG = 
+static const i2s_config_t I2S_CONFIG =
 {
      .mode = I2S_MODE_SLAVE | I2S_MODE_RX,
      .sample_rate = CONFIG_SOUND_SAMPLE_FREQUENCY,
@@ -54,7 +61,7 @@ static const i2s_config_t I2S_CONFIG =
      .use_apll = 0
 };
 
-static const i2s_pin_config_t I2S_PIN_CONFIG = 
+static const i2s_pin_config_t I2S_PIN_CONFIG =
 {
     .bck_io_num = 23,
     .ws_io_num = 39,
@@ -86,7 +93,7 @@ static void initAdc()
 static void initializeUdpMessageHeader()
 {
     *(uint32_t*)udpMessageData = htonl(UDP_MESSAGE_ID);
-    *(uint32_t*)(udpMessageData + 4) = 
+    *(uint32_t*)(udpMessageData + UDP_MESSAGE_CURRENT_PAYLOAD_SIZE_OFFSET) =
         htonl(UDP_MESSAGE_PAYLOAD_HEADER_SIZE + CONFIG_SOUND_MESSAGE_SAMPLE_COUNT * sizeof(int32_t));
     
     udpSampleData = (int32_t*)(udpMessageData + UDP_MESSAGE_FULL_HEADER_SIZE);
@@ -96,18 +103,18 @@ static void updateUdpMessageIdAndTimestamp()
 {
     static uint16_t currentId = 0;
     
-    *(uint16_t*)(udpMessageData + 8) = htons(currentId);
+    *(uint16_t*)(udpMessageData + UDP_MESSAGE_CURRENT_ID_OFFSET) = htons(currentId);
 
     struct timeval tv;
     struct tm timeinfo;
     gettimeofday(&tv, NULL);
     localtime_r(&tv.tv_sec, &timeinfo);
 
-    udpMessageData[10] = (uint8_t)timeinfo.tm_hour;
-    udpMessageData[11] = (uint8_t)timeinfo.tm_min;
-    udpMessageData[12] = (uint8_t)timeinfo.tm_sec;
-    *(uint16_t*)(udpMessageData + 13) = htons(tv.tv_usec / US_IN_MS_COUNT);
-    *(uint16_t*)(udpMessageData + 15) = htons(tv.tv_usec % US_IN_MS_COUNT);
+    udpMessageData[UDP_MESSAGE_CURRENT_HOUR_OFFSET] = (uint8_t)timeinfo.tm_hour;
+    udpMessageData[UDP_MESSAGE_CURRENT_MINUTE_OFFSET] = (uint8_t)timeinfo.tm_min;
+    udpMessageData[UDP_MESSAGE_CURRENT_SECOND_OFFSET] = (uint8_t)timeinfo.tm_sec;
+    *(uint16_t*)(udpMessageData + UDP_MESSAGE_CURRENT_MS_OFFSET) = htons(tv.tv_usec / US_IN_MS_COUNT);
+    *(uint16_t*)(udpMessageData + UDP_MESSAGE_CURRENT_US_OFFSET) = htons(tv.tv_usec % US_IN_MS_COUNT);
 
     currentId++;
 }
